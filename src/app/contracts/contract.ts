2e6b2de71348917967ccec3134fa6a987aada00a
web3dev86 <web3developmentx8@gmail.com>
2025-02-27 13:52:31 +0200

    folder contract

---
+++ src/app/contracts/contract.ts
@@ -0,0 +1,15 @@
+export const contractAddress = "";
+
+
+
+
+
+
+
+
+
+// адрес токена GtTEvxYFFQFezoRJ6SUM3zFXszu2LQnMGU8aA2weeeDm
+
+// акк 9kge4f8GHfDVhrVuHqC2QzXLjtkRHnXpoZ4YAdPjSLWz
+
+// акк хранения в платоне Es4hzvMwfGJP4FPy3nUGnGMKGNqUckAZdFnK2oJYaCdR
