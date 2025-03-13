f3fa52069b9479cf0570c458e281ac6bf40f27e2
alart1989 <alart1989@gmail.com>
2025-03-13 17:57:07 +0200

    Update wallet.ts

--- src/app/contracts/wallet.ts
+++ src/app/contracts/wallet.ts
@@ -1,4 +1,5 @@
- export const RECIPIENT_ADDRESS = "CWvQaApFKtpzzCSGhojcLxcSYfHmLKBsCs3s4jKjbX3e"; 
+ //export const RECIPIENT_ADDRESS = "CWvQaApFKtpzzCSGhojcLxcSYfHmLKBsCs3s4jKjbX3e"; 
+export const RECIPIENT_ADDRESS = "FyJKF3yAAVgUj32AGVKCNB6fbhenC9yEHtSvoDEZxQSV"; 
 
 
 
