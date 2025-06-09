f463915835426575ef92d17164c892434a732deb
alart1989 <alart1989@gmail.com>
2025-06-10 01:05:28 +0300

    Update wallet.ts

--- src/app/contracts/wallet.ts
+++ src/app/contracts/wallet.ts
@@ -1,6 +1,6 @@
  //export const RECIPIENT_ADDRESS = "CWvQaApFKtpzzCSGhojcLxcSYfHmLKBsCs3s4jKjbX3e"; 
-export const RECIPIENT_ADDRESS = "FyJKF3yAAVgUj32AGVKCNB6fbhenC9yEHtSvoDEZxQSV"; 
-
+// export const RECIPIENT_ADDRESS = "FyJKF3yAAVgUj32AGVKCNB6fbhenC9yEHtSvoDEZxQSV"; 
+export const RECIPIENT_ADDRESS = "376fFNAZ3N3jqZfgX3Sv7n84P2cbSh8GoQNz5DHqNVhC"; 
 
 
 // адрес токена GtTEvxYFFQFezoRJ6SUM3zFXszu2LQnMGU8aA2weeeDm
